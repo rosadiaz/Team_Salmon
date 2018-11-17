ba1888aa5df24a5bda1b921ea819662e52d2b395
Dan Hawkins <danhawkins4@gmail.com>
2018-11-16 20:09:36 -0800

    add sessions sign_in , but a bit buggy..

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,32 @@ def new
 end 
 
 def create
-    user = User.find_by_email params[:email]
-    sesions[:user_id] = user.id
-    redirect_to root_path, notice: "Signed in!"
+    @user = User.find_by_email params[:session][:email]
+
+    if @user&.authenticate(session_params[:password])
+        session[:user_id] = @user.id
+        redirect_to root_path, notice: "Signed in!"
+
+    else 
+        puts "didn't log in ======================"
+        flash.now[:danger] = "Wrong credz, loser"
+        
+        redirect_to new_session_path, notice: "Wrong Credz!"
+
+    
+    end 
 end 
 
 
+
+def destroy 
+    session[:user_id] =nil 
+    redirect_to root_path, notice: "Signed out!"
+end 
+
 private 
 def session_params 
-    params.require(:session).permit(:nickname, :password)
+    params.require(:session).permit(:email, :password)
 end 
 
 end
