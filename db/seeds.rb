6faee10220ea67369887289889762ea2a281bc8d
chrischu5 <chris.chu.yc@gmail.com>
2018-11-17 12:51:11 -0800

    Update gemfile and start seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Question.delete_all
+Quiz.delete_all
+User.delete_all
+
+PASSWORD = "superadmin"
+NUM_OF_CREATES = 100
+
+super_user = User.create(
+  first_name: "Test",
+  last_name: "User",
+  email: "test@user.com",
+  password: PASSWORD,
+  admin: true
+)
