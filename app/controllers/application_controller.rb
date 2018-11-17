b03c74add23fbb1a3f599c7800f66f6397996129
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-11-17 11:26:36 -0800

    Session destroy and current_user instance method bug fix
    Adds session destroy, greets user when signed in at welcome page, remove instance caller in application controller current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     
     def current_user
         if session[:user_id].present?
-          @current_user ||= User.find(session[:user_id])
+          current_user ||= User.find_by_id(session[:user_id])
         end
       end
       helper_method :current_user
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :sessions, only: [:new, :create, :destroy]
 
   root({ to: 'home#index' })
   resources :quizzes 
