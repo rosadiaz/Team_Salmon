a92559a6cf2bad35307850ae057e77e0eaf60d3c
Dan Hawkins <danhawkins4@gmail.com>
2018-11-16 18:22:46 -0800

    new and create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
 #    validates :first_name, presence: true,
 #    validates :last_name, presence: true,
-validates :nickname, presence: true, uniqueness: true,
+validates :nickname, presence: true, uniqueness: true
 validates :email, presence: true, 
    uniqueness: true,
    format: VALID_EMAIL_REGEX
