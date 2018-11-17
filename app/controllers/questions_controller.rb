662d602b60f3cbfd024141cc59bc49c5a3446ee8
Ronald Liu <rkkl90@gmail.com>
2018-11-16 18:02:39 -0800

    Added actions to Questions Controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,46 @@
 class QuestionsController < ApplicationController
-    
+    before_action :find_quiz, only: [:create]
+    before_action :find_question only: [:destroy]
+
     def new
+        @question = Question.new
     end
 
     def create
+
+        @question = Question.new question_params
+        @question.quiz = find_quiz
+
+        if @question.save
+            # UPDATE DOM WITH NEW QUESTION
+        else
+            # FLASH ERROR
+        end
     end
 
     def edit
     end
 
     def update
+        if @question.update question_params
+            # REFRESH THE DOM TO SHOW CHANGES
+        else
+            # FLASH ERROR
+        end
+    end
+
+    def destroy
+        @question.destroy
+        # AFTER DESTROY REFRESH DOM TO SHOW DELETION
+    end
+
+    private
+    def find_quiz
+        @quiz = Quiz.find params[:quiz_id]
+    end
+
+    def find_question
+        @question = Question.find params[:id]
     end
 
 end
