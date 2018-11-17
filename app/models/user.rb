a0334ecc9635156d2c8f4e3b2c7fc6b30efa7164
Dan Hawkins <danhawkins4@gmail.com>
2018-11-16 17:59:16 -0800

    create user controller

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    has_secure_password 
+
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+#    validates :first_name, presence: true,
+#    validates :last_name, presence: true,
+validates :nickname, presence: true, uniqueness: true,
+validates :email, presence: true, 
+   uniqueness: true,
+   format: VALID_EMAIL_REGEX
+end
