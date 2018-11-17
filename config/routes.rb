47396fba1f0bf1ad2da73111db3988d34b0c09d2
Dan Hawkins <danhawkins4@gmail.com>
2018-11-17 11:24:32 -0800

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show, :destroy, :update]
+
   resources :sessions, only: [:new, :create, :destroy]
 
   root({ to: 'home#index' })
