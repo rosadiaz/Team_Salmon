b98c18363fde1e08bd66378bcbabc1e685953f30
Ronald Liu <rkkl90@gmail.com>
2018-11-17 14:25:32 -0800

    Implement abilities for editing and taking a quiz and displayed questions and function buttons on the quiz show page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
-    before_action :find_quiz, only: [:new, :create]
-    before_action :find_question, only: [:destroy]
+    before_action :find_quiz, only: [:new, :edit, :update, :create]
+    before_action :find_question, only: [:show, :edit, :update, :destroy]
 
     def new
         @question = Question.new
@@ -21,24 +21,32 @@ class QuestionsController < ApplicationController
         end
     end
 
+    def show
+        render :show
+    end
+
     def edit
+
     end
 
     def update
         if @question.update question_params
-            # REFRESH THE DOM TO SHOW CHANGES
+            redirect_to quiz_path(@quiz.id)
         else
-            # FLASH ERROR
+            render :edit
         end
     end
 
     def destroy
         @question.destroy
-        # AFTER DESTROY REFRESH DOM TO SHOW DELETION
+        # AFTER REDIRECT TO SOMEWHERE
     end
 
     private
-    
+    def question_params
+        params.require(:question).permit(:title, :option0, :option1, :option2, :option3, :correct_answer)
+    end
+
     def find_quiz
         @quiz = Quiz.find params[:quiz_id]
     end
