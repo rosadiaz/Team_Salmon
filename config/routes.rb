87934ab46efb28f6661d84a9cc231865aec4cc66
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-11-17 16:58:26 -0800

    Quiz index view adjusts according different media size

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
-  resources :users, only: [:new, :create]
-=======
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
 
->>>>>>> origin/integration
   resource :sessions, only: [:new, :create, :destroy]
 
   resources :quizzes do
