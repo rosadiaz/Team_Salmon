c0cdbb3c9279d39a3fa1cb000ad67766058a0f41
Ronald Liu <rkkl90@gmail.com>
2018-11-17 19:41:46 -0800

    Implement results show page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,6 @@
 class ResultsController < ApplicationController
 
-    before_action :find_quiz, only: [ :create ]
+    before_action :find_quiz, only: [ :create, :show ]
     before_action :authorize_user!, only: [ :create ]
   
     def create
@@ -9,13 +9,15 @@ class ResultsController < ApplicationController
         @result = Result.new(user: current_user, quiz: @quiz, score: @score)
         if @result.save
             flash[:success] = "Answers Submited"
-            redirect_to root_path
+            redirect_to quiz_result_path(@quiz.id, @result.id)	
         else
             flash[:danger] = @result.errors.full_messages.join(", ");
         end
     end
     
     def show
+        @result = Result.find params[:id]
+        @max_score = @quiz.questions.length
     end
 
     private
