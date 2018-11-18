3cd364f97e6ac9ed71e6422455c2a8ca2e349465
Orsolya Torokne Bus <orsolyabus@gmail.com>
2018-11-17 19:54:28 -0800

    implement clear_data

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,2 +1,17 @@
 module ResultsHelper
+
+  def calculate_score(answers)
+    quiz_id = answers.quiz_id
+    submitted_answers = clear_data(answers)
+    correct_answes = get_correct_answers(quiz_id)
+    hits = compare_answers(correct_answers, submitted_answers)
+    score = get_score(hits) # we can add the difficoulty here
+  end
+
+  def clear_data(answers)
+    to_reject = [ :authenticity_token, :controller, :action, :quiz_id]
+    answers.reject { |k| to_reject.include?(k) }
+  end
+
+
 end
