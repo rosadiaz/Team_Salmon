ceaa7e44bf3254d317e52b9defcd69acc39dfa14
chrischu5 <chris.chu.yc@gmail.com>
2018-11-17 18:26:42 -0800

    Fix redirect paths for user sign-in and log-in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "Thank you for signing up!"
+      flash[:success] = "Thank you for signing up!"
+      redirect_to quizzes_path
     else
       render :new
     end
