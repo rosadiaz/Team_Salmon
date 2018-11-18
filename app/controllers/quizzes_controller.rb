cbff1542a1fccb109bd072d0b2624f79a29f0d76
Dan Hawkins <danhawkins4@gmail.com>
2018-11-18 15:16:41 -0800

    only display quizzes with questions, and display username on quiz show page

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -26,7 +26,8 @@ class QuizzesController < ApplicationController
   end
   
   def index
-    @quizzes = Quiz.all.order(created_at: :desc)
+    quizzes = Quiz.all.order(created_at: :desc)
+    @valid_quizzes = quizzes.select{|quiz| quiz.questions.length>0}
   end
 
   def edit
